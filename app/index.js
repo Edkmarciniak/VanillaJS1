1e54921a950205f4594eb352ef8c1c1a376b8524
Ed Marciniak <ekmarciniak@gmail.com>
2022-12-09 11:45:21 -0600

    Update push

--- app/index.js
+++ app/index.js
@@ -1,47 +1,3 @@
-//const product = {
-  name: "Socks",
-  price: 5.99,
-  description: "A pair of warm, fuzzy socks",
-  imgUrl:
-    "https://images.unsplash.com/photo-1556740732-3f8f0a6c7b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-};
-
-function createProductTile(prod) {
-  return `
-  <figure>
-      <img src="${product.imgUrl}" alt="${product.name}">
-      <figcaption>
-        <h2>${product.name}</h2>
-        <small>${product.price}</small>
-        <p>${product.description}</p>
-      </figcaption>
-    </figure>
-  `;
-}
-
-const productTile = createProductTile(product);
-console.log(productTile);
-
-
-
-
-
-
-
-SArrays are collections that are indexed by implicitly generated numbers
-// The first item in the array is at index 0, the second at index 1, and so on
-// Arrays are zero-indexed, meaning the first item is at index 0
-const nums = ["socks", "shoes", "pants", "shirt", "hat"];
-
-const firstItem = nums[0];
-const secondItem = nums[1];
-const thirdItem = nums[2];
-
-console.log(firstItem); // socks
-console.log(secondItem); // shoes
-console.log(thirdItem); // pants
-
-const catalog = [];
 const products = [
   {
     name: "Socks",
@@ -65,3 +21,26 @@ const products = [
       "https://images.unsplash.com/photo-1556740732-3f8f0a6c7b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
   },
 ];
+
+function createProductTile(prod) {
+  return `
+  <figure>
+      <img src="${prod.imgUrl}" alt="${prod.name}">
+      <figcaption>
+        <h2>${prod.name}</h2>
+        <small>${prod.price}</small>
+        <p>${prod.description}</p>
+      </figcaption>
+    </figure>
+  `;
+}
+
+const catalogMarkup = `
+  <main>
+    ${createProductTile(products[0])}
+    ${createProductTile(products[1])}
+    ${createProductTile(products[2])}
+  </main>
+`;
+
+console.log(catalogMarkup);
